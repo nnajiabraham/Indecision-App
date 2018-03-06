9fb59d36663135b1b1947ac0f94ea46d4f2a38c0
Abraham Nnaji <nnajiabraham@gmail.com>
2018-03-05 18:04:41 -0600

    Add files via upload

---
+++ src/components/Option.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Option = (props) =>(
+  <div className = "option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
+
+    <button
+      className = "button button--link"
+      onClick={() => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+
+      Remove
+    </button>
+  </div>
+);
+
+export default Option;
+
+// class Option extends React.Component {
+//   render() {
+//     return(
+//       <div>
+//         {this.props.optionText}
+//       </div>
+//     );
+//
+//   }
+// }
